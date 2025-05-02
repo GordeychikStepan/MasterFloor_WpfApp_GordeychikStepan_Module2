a84ef4155c21df71b879d99943eb45a69875119b
GordeychikStepan <gordeychik.stepan@mail.ru>
2025-05-02 14:33:19 +0500

    Модуль 1 и 2

---
+++ VS/TEST/PercentCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TechGear_WpfApp_Test1;
+
+namespace TestProject2
+{
+    [TestFixture]
+    public class PercentCalculatorTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(9999, 0)]
+        [TestCase(10000, 5)]
+        [TestCase(50000, 10)]
+        [TestCase(300001, 15)]
+        public void GetPercent_ReturnsExpected(int totalSale, int expected)
+        {
+            int actual = PercentCalculator.GetPercent(totalSale);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
